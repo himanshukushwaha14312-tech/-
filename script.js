a9f2d21719e4320ab45bc69808d69cf7b86c69df
himanshukushwaha14312-tech <himanshukushwaha14312@gmail.com>
2026-04-07 23:37:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-let started = false;
-
 function login() {
-    let u = document.getElementById("user").value;
-    let p = document.getElementById("pass").value;
+    let u = document.getElementById("user").value.trim();
+    let p = document.getElementById("pass").value.trim();
 
     if(u === "himanshu" && p === "annu") {
         window.location.href = "home.html";
-            started = true;
-        }
-
     } else {
         document.getElementById("error").innerText = "Wrong ❌";
     }
-}
-
-/* ❤️ Hearts Animation */
-function startHearts() {
-    setInterval(() => {
-        let heart = document.createElement("div");
-        heart.className = "heart";
-        heart.innerText = "❤️";
-
-        heart.style.left = Math.random() * 100 + "vw";
-        heart.style.fontSize = Math.random() * 20 + 20 + "px";
-
-        document.body.appendChild(heart);
-
-        setTimeout(() => {
-            heart.remove();
-        }, 5000);
-
-    }, 300);
 }
