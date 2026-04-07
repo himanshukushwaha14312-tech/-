06552c8652175d02f218ceba4e4120f446213388
himanshukushwaha14312-tech <himanshukushwaha14312@gmail.com>
2026-04-07 23:54:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-alert("JS Connected");
-function login() {
-    let u = document.getElementById("user").value.trim();
-    let p = document.getElementById("pass").value.trim();
+function login(){
+ let u = document.getElementById("user").value.trim().toLowerCase();
+ let p = document.getElementById("pass").value.trim();
 
-    if(u === "himanshu" && p === "annu") {
-        window.location.href = "home.html";
-    } else {
-        document.getElementById("error").innerText = "Wrong ❌";
-    }
+ if(u === "himanshu" && p === "annu"){
+  sessionStorage.setItem("auth","true");
+  window.location.href = "home.html";
+ }else{
+  document.getElementById("msg").innerText = "❌ Wrong Username or Password";
+ }
+}
+
+// PROTECT HOME PAGE
+if(location.pathname.includes("home.html")){
+ if(sessionStorage.getItem("auth") !== "true"){
+  location.href = "index.html";
+ }
 }
