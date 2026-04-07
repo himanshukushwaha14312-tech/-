788286469f5d158323b1fae6c23b25e12d6f6473
himanshukushwaha14312-tech <himanshukushwaha14312@gmail.com>
2026-04-07 23:41:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-function login() {
-    let u = document.getElementById("user").value.trim();
-    let p = document.getElementById("pass").value.trim();
+function login(){
+ let u=document.getElementById("user").value;
+ let p=document.getElementById("pass").value;
 
-    if(u === "himanshu" && p === "annu") {
-        window.location.href = "home.html";
-    } else {
-        document.getElementById("error").innerText = "Wrong ❌";
-    }
+ if(u=="himanshu" && p=="annu"){
+  sessionStorage.setItem("auth","true");
+  window.location="home.html";
+ }else{
+  document.getElementById("msg").innerText="Wrong ❌";
+ }
 }
+
+// PROTECT
+if(location.pathname.includes("home.html")){
+ if(sessionStorage.getItem("auth")!="true"){
+  location.href="index.html";
+ }
+}
+
+// MUSIC
+function toggleMusic(){
+ let m=document.getElementById("music");
+ m.paused?m.play():m.pause();
+}
+
+// CONFETTI
+function fire(){
+ confetti({particleCount:200,spread:120});
+}
+
+// TYPING
+let text="Tum meri sabse special ho ❤️";
+let i=0;
+
+function typing(){
+ if(i<text.length){
+  document.getElementById("typing").innerHTML+=text[i];
+  i++;
+  setTimeout(typing,50);
+ }
+}
+
+window.onload=typing;
